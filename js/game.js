3aeb0467e9e6374c31e04e1c94926c31855ff46e
Chloe Havel <113055595+Chloe-Havel@users.noreply.github.com>
2022-11-08 21:26:16 -0500

    s

---
+++ js/game.js
@@ -0,0 +1,21 @@
+/* global phaser */ 
+
+// Copyright (c) 2020 Chloe Havel All rights reserved
+//
+// Created by: Chloe Havel
+// Created on: Sep 2020
+// This is the phaser3 game configation file
+
+/**
+ * Start Phaser Game.
+ */
+const config = {
+    type: Phaser.AUTO,
+    with: 1920
+    height: 1080,
+    // set background color
+    backgroundColor: 0x5f6e7a,
+};
+
+const game = new Phaser.Game(config);
+console.log(game);
