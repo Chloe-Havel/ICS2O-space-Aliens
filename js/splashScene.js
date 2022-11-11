85654324648394ab9b7d9e3096a6558524dd6069
Chloe-Havel <Chloe-Havel@users.noreply.github.com>
2022-11-11 19:22:52 +0000

    Prettified Code!

--- js/splashScene.js
+++ js/splashScene.js
@@ -10,47 +10,47 @@
  * This class is the Splash Scene.
  */
 class SplashScene extends Phaser.Scene {
-/**
- * This method is the construtor.
- */
-constructor(){
-    super({ key: "splashScene"})
-}
+  /**
+   * This method is the construtor.
+   */
+  constructor() {
+    super({ key: "splashScene" })
+  }
 
-/**
- * Canbe definded on your own Ssenes.
- * This method is called by the Scene Manager when the scene Starts,
- *   before preload() and create().
- * @param {object} data - Anydata passed via ScenePlugin.add() or ScenePlugin.start().
- */
-Infinity(data) {
+  /**
+   * Canbe definded on your own Ssenes.
+   * This method is called by the Scene Manager when the scene Starts,
+   *   before preload() and create().
+   * @param {object} data - Anydata passed via ScenePlugin.add() or ScenePlugin.start().
+   */
+  Infinity(data) {
     this.cameras.main.setBackgroundColor("ffffff")
-}
+  }
 
-/**
- * Can be defined on yourown Scenes.
- * Use it to load assests.
- */
-perload() {
+  /**
+   * Can be defined on yourown Scenes.
+   * Use it to load assests.
+   */
+  perload() {
     console.log("Splash Scene")
-}
+  }
 
-/**
- * cand be defined on your own Scenes.
- * Use it to create your game objects.
- * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
- */
-create(data) {
+  /**
+   * cand be defined on your own Scenes.
+   * Use it to create your game objects.
+   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
+   */
+  create(data) {
     // pass
-}
+  }
 
-/**
- * Should be overridden  by your own Scenes.
- * This method is called once per game step white the scene is runing
- * @param {number} time - The current time.
- * @param {number} delta - The delta time in ms sinec last frame.
- */
-update(time, delta) {
+  /**
+   * Should be overridden  by your own Scenes.
+   * This method is called once per game step white the scene is runing
+   * @param {number} time - The current time.
+   * @param {number} delta - The delta time in ms sinec last frame.
+   */
+  update(time, delta) {
     // pass
-    }
+  }
 }
