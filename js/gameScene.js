e0fb25374a51e4fb00385804f04ef8dc230e2f40
Chloe Havel <113055595+Chloe-Havel@users.noreply.github.com>
2022-11-30 14:04:54 -0500

    ??

--- js/gameScene.js
+++ js/gameScene.js
@@ -73,10 +73,9 @@ class GameScene extends Phaser.Scene {
 
     if (KeyRightgObj.isDown === true) {
       this.ship.x =+ 15
-      if (this.ship.x > 1920) {
+      if (this.ship.x > 1920) }
         this.ship.x = 1920
-    }
-  }
+      }
 }
 
 export default GameScene
