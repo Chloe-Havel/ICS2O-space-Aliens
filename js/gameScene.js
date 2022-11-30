364623e815d53ee56c4e6a57afe42990ec6f3bfd
Chloe Havel <113055595+Chloe-Havel@users.noreply.github.com>
2022-11-30 13:46:34 -0500

    ?

--- js/gameScene.js
+++ js/gameScene.js
@@ -60,12 +60,13 @@ class GameScene extends Phaser.Scene {
    * @param {number} delta - The delta time in ms sinec last frame.
    */
   update(time, delta) {
+    // called 6- times a scond, hopefully!
     const KeyLeftObj = this.input.Keyboard.addkey("LEFT")
     const KeyRightObj = this.input.Keyboard.addkey("RIGHT")
   }
 }
 
-if (KeyRightObj.isDown == true)
+if (KeyRightObj.isDown === true)
   this.ship.x += 15
   if (this.ship.x> 1920) {
     this.ship.x = 1920
