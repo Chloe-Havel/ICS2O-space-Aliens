e76fd93a8ff51db15f42ea85e488bcdb7f505e0e
Chloe Havel <113055595+Chloe-Havel@users.noreply.github.com>
2022-11-30 13:59:20 -0500

    /

--- js/gameScene.js
+++ js/gameScene.js
@@ -65,11 +65,16 @@ class GameScene extends Phaser.Scene {
     const KeyRightObj = this.input.Keyboard.addkey("RIGHT")
 
     if (KeyLeftObj.isDown === true) {
-      this.ship.x = this.ship.x - 15
+      this.ship.x += - 15
+      if (this.ship.x <0) {
+        this.ship.x = 0
     }
+  }
 
     if (KeyRightgObj.isDown === true) {
-      this.ship.x = this.ship.x - 15
+      this.ship.x =+ 15
+      if (this.ship.x > 1920) {
+        this.ship.x = 1920
     }
   }
 }
