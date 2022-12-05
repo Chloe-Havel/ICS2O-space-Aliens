6bc5a6fcdd1a216ec02c6ca90e2e2ff701821c08
Chloe Havel <113055595+Chloe-Havel@users.noreply.github.com>
2022-12-05 14:50:11 -0500

    coninue where you left off

--- js/gameScene.js
+++ js/gameScene.js
@@ -10,59 +10,43 @@
  * This class is the Splash Scene.
  */
 class GameScene extends Phaser.Scene {
-  /**
-   * This method is the construtor.
-   */
-  constructor() {
+  constructor () {
     super({ key: "gameScene" })
 
     this.setBackground = null
     this.ship = null
+    this.fireMissile = null
   }
 
-  /**
-   * Can be definded on your own Ssenes.
-   * This method is called by the Scene Manager when the scene Starts,
-   *   before preload() and create().
-   * @param {object} data - Anydata passed via ScenePlugin.add() or ScenePlugin.start().
-   */
   init(data) {
     this.cameras.main.setBackgroundColor("ffffff")
   }
 
-  /**
-   * Can be defined on your own Scenes.
-   * Use it to create yur game objects.
-   */
   preload() {
     console.log("Game Scene")
 
+    // images
     this.load.image("startBackground", "./assets/starBackground.png")
     this.load.image("ship", "./assets/spaceShip.png")
+    this.load.image("missile", "assets/missile.png")
   }
 
-  /**
-   * can be defined on your own Scenes.
-   * Use it to create your game objects.
-   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
-   */
   create(data) {
     this.background = this.add.image(0, 0, "startBackground").setScale(2.0)
     this.background.setOrigin(0, 0)
 
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
+
+    // create a group for the missiles
+    this.missileGroup = this.physics.add.group()
   }
 
-  /**
-   * Should be overridden  by your own Scenes.
-   * This method is called once per game step white the scene is runing
-   * @param {number} time - The current time.
-   * @param {number} delta - The delta time in ms sinec last frame.
-   */
   update(time, delta) {
     // called 6- times a scond, hopefully!
+
     const keyLeftObj = this.input.keyboard.addKey("LEFT")
     const keyRightObj = this.input.keyboard.addKey("RIGHT")
+    const KwySpaceObj = this.input.keyboard.addkey("SPACE")
 
     if (keyLeftObj.isDown === true) {
       this.ship.x += -15
@@ -76,6 +60,9 @@ class GameScene extends Phaser.Scene {
       if (this.ship.x > 1920) {
         this.ship.x = 1920
       }
+    }
+
+    
     }
   }
 }
