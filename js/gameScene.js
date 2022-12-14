cd48fa781c1276a4c6829a02ef08248cce4a73d2
Chloe Havel <113055595+Chloe-Havel@users.noreply.github.com>
2022-12-14 10:32:46 -0500

    added a nother funcsion

--- js/gameScene.js
+++ js/gameScene.js
@@ -57,10 +57,7 @@ class GameScene extends Phaser.Scene {
     this.createAlien()
 
     //Collision between missiles and aliens
-    this.physics.add.collider(
-      this.missileGroup,
-      this.alienGroup,
-      function (missileCollide) {
+    this.physics.add.collider( this.missileGroup, this.alienGroup, function (missileCollide, alienCollide) {
         alienCollide.destroy()
         missileCollide.destroy()
         this.sound.play("explosion")
