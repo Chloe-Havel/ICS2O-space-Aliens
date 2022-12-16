31b8629209dcf1d71252c9ad46b5c7e88425df8c
Chloe Havel <113055595+Chloe-Havel@users.noreply.github.com>
2022-12-16 17:13:39 -0500

    removed the things that i just added

--- js/gameScene.js
+++ js/gameScene.js
@@ -46,15 +46,7 @@ class GameScene extends Phaser.Scene {
   create(data) {
     this.background = this.add.image(0, 0, "startBackground").setScale(2.0)
     this.background.setOrigin(0, 0)
-
-    this.scoreText +
-      this.add.text(
-        10,
-        10,
-        "score: " + this.score.toString(),
-        this.scoreTextStyle
-      )
-
+    
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
 
     // create a group for the missiles
@@ -72,8 +64,6 @@ class GameScene extends Phaser.Scene {
         alienCollide.destroy()
         missileCollide.destroy()
         this.sound.play("explosion")
-        this.score + this.score + 1
-        this.scoreText.setText("Score: " + this.score.toString())
         this.createAlien()
         this.createAlien()
         console.log("Hit")
