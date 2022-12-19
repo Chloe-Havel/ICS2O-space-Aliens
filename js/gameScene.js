3bd73079cfe48f77db498bb4f9d718025db5ee11
Chloe-Havel <Chloe-Havel@users.noreply.github.com>
2022-12-19 14:10:22 +0000

    Prettified Code!

--- js/gameScene.js
+++ js/gameScene.js
@@ -7,7 +7,6 @@
 // This is the Splash Scene
 
 class GameScene extends Phaser.Scene {
-  
   // create an alien
   createAlien() {
     const alienXLocation = Math.floor(Math.random() * 1920) + 1 // this will get a number between 1 and 1920
@@ -26,7 +25,11 @@ class GameScene extends Phaser.Scene {
     this.fireMissile = false
     this.score = 0
     this.scoreText = null
-    this.scoreTexttyle = { front: "65px Arial", fill: "#ffffff", align: "center"}
+    this.scoreTexttyle = {
+      front: "65px Arial",
+      fill: "#ffffff",
+      align: "center",
+    }
   }
 
   init(data) {
@@ -50,7 +53,12 @@ class GameScene extends Phaser.Scene {
     this.background = this.add.image(0, 0, "startBackground").setScale(2.0)
     this.background.setOrigin(0, 0)
 
-    this.scoretText = this.add.text(10, 10, "score: " + this.score.toString(), this.scoreTexttyle)
+    this.scoretText = this.add.text(
+      10,
+      10,
+      "score: " + this.score.toString(),
+      this.scoreTexttyle
+    )
 
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
 
@@ -62,7 +70,10 @@ class GameScene extends Phaser.Scene {
     this.createAlien()
 
     //Collision between missiles and aliens
-    this.physics.add.collider(this.missileGroup, this.alienGroup, function (missileCollide, alienCollide) {
+    this.physics.add.collider(
+      this.missileGroup,
+      this.alienGroup,
+      function (missileCollide, alienCollide) {
         alienCollide.destroy()
         missileCollide.destroy()
         this.sound.play("explosion")
